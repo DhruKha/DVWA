55ac8662d02bacb82f958120e73c5d4bf89caec8
dhrukha <020320two@gmail.com>
2025-10-18 19:46:57 +1100

    enhance csp

--- vulnerabilities/csp/source/high.php
+++ vulnerabilities/csp/source/high.php
@@ -1,5 +1,5 @@
 <?php
-$headerCSP = "Content-Security-Policy: script-src 'self' secuarden.com sefficien.com;";
+$headerCSP = "Content-Security-Policy: script-src 'self' secuarden.com sefficien.com smartbriefs.com;";
 
 header($headerCSP);
 
